ff18b96c667fc8a0170216eb652c2d13a93df2a4
Dillon Fernando <dfernando1120@gmail.com>
2017-01-25 20:50:35 -0500

    Initial Commit

---
+++ Tutorial-NSCacheTests/Tutorial_NSCacheTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tutorial_NSCacheTests.swift
+//  Tutorial-NSCacheTests
+//
+//  Created by Dillon Fernando on 1/25/17.
+//  Copyright © 2017 Dillon Fernando. All rights reserved.
+//
+
+import XCTest
+@testable import Tutorial_NSCache
+
+class Tutorial_NSCacheTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
